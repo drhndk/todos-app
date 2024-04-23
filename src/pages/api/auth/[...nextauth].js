9bd530acfdb12cb1326582cf71812e74c616da2e
drhndk <dhurhankusnaidi@gmail.com>
2024-04-23 18:45:26 +0700

    first commit

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,33 +1,15 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google";
-export default NextAuth({
-  session: {
-    strategy: 'jwt'
-  },
-  secret: process.env.NEXT_SECRET_JWT,
+import GoogleProvider from "next-auth/providers/google"
+
+export const authOptions = {
+  // Configure one or more authentication providers
   providers: [
-     GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.NEXT_GOOGLE_CLIENT_SECRET
-      })
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     // ...add more providers here
   ],
-  callbacks: {
-    async jwt({token,account,user}) {
-      if (account?.provider === 'google') {
-        const data = {
-          name: user.name,
-          email: user.email,
-          image: user.image,
-        }
-        token.name = data.name,
-        token.email = data.email,
-        token.image = data.image
-      }
-      return token
-    },
-  },
-  pages : {
-    signIn: "/login",
-  }
-});
+}
+
+export default NextAuth(authOptions)
